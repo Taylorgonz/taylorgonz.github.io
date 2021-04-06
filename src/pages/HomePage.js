3bd1690f51dc0e1fe47dbdd5c0200a7d652ba6c8
Taylor Gonzales <hello@taylorgonz.com>
2021-04-05 22:16:16 -0400

    home hero styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,7 +11,7 @@ function HomePage(props) {
         <div className='homePage'>
             <HeroHome title={props.title} subTitle={props.subTitle} text={props.text} />
             <h1 className="heroTitle text-center display-3 mb-5">Web Projects</h1>
-            <Carousel/>
+            <Carousel/>       
         </div>
     );
 }
